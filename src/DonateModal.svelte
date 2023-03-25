30446e17de7cb4611adad981b27a8ec0f5c7503f
Joe Bott <jjbott@gmail.com>
2023-03-25 12:47:56 -0500

    Remove the `supporters.json` box

--- src/DonateModal.svelte
+++ src/DonateModal.svelte
@@ -1,7 +1,4 @@
-<script>
-  import Supporters from './Supporters.svelte'
-</script>
+<!-- TODO: Rename. This has nothing to do with donations anymore. Maybe rename the "SUPPORT" header as well. -->
+<!-- TODO: Add more info here. News? Feedback/Bug report method? -->
 <p class="mb-3">Thanks to svt-heardle.glitch.me for the remixable copy of Heardle.app.</p>
-<div class="pt-6">
-  <Supporters/>
-</div>
+<p class="mb-3">And obviously thank you to the Johns for 40 years (and counting) of great music!</p>
