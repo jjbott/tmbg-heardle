7c7e1bc97c8111adfb11d65d2cfec037d2b1212a
Joe Bott <jjbott@gmail.com>
2023-11-19 19:33:03 -0600

    Add button to TMBG shop, instead of dead link to kofi

--- src/InfoModal.svelte
+++ src/InfoModal.svelte
@@ -1,17 +1,21 @@
+<script>
+    import TmbgShop from "./TmbgShopButton.svelte";
+</script>
+
 <div class="text">
+    <p class="mb-3">A clone of Heardle (now defunct) but for songs by the band They Might Be Giants.</p>
+
+    <p class="mb-3">Each TMBG Heardle is randomly chosen from TMBG's discography.</p>
+    <p />
     <p class="mb-3">
-        A clone of Heardle (now defunct) but for songs by the band They Might Be Giants.
-    </p> 
-
-    <p class="mb-3">Each TMBG Heardle is randomly chosen from TMBG's discography (and I will probably add tracks from the Johns' side projects at some point.)</p> 
-
-
-
-     <p />  
-
-
-
-      <p class="text-xs mb-3 text-custom-line">
+        Having problems? Create an issue on <a href="https://github.com/jjbott/tmbg-heardle">Github</a>!
+    </p>
+    <p />
+    <p class="mb-3">
+        <TmbgShop />
+    </p>
+    <p />
+    <p class="text-xs mb-3 text-custom-line">
         Prepared with <a href="https://developers.soundcloud.com">Soundcloud</a>,
         <a href="https://svelte.dev">Svelte</a>,
         <a href="https://tailwindcss.com">Tailwind</a>,
@@ -20,11 +24,12 @@
         <a href="https://fonts.google.com/noto/specimen/Noto+Sans">Noto Sans</a>,
 
         <a href="https://iconsvg.xyz">IconSVG</a>, <a href="https://momentjs.com">momentjs</a>,
-        
+
         <a href="https://tarekraafat.github.io/autoComplete.js/#/">autocomplete.js</a>, and powered by
         <a href="https://glitch.com/">Glitch</a>. Original Heardle by
         <a href="https://omakase.studio" title="Studio Omakase">Studio omakase / お任せ</a>. TMBG version is currently
         maintained by <a href="https://twitter.com/ZorMonkey">Joe</a>. Original was created by
         <a href="https://twitter.com/ckolderup">casey</a>
     </p>
+    <p class="text-xs mb-3 text-custom-line">Not affiliated with They Might Be Giants</p>
 </div>
---
+++ src/TmbgShopButton.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { ga } from "@beyonk/svelte-google-analytics";
+</script>
+
+<div class="text-center flex justify-center items-center flex-col">
+    <a
+        class="tmbgshop-button py-2 px-3 rounded-lg items-center flex svelte-1d3p4dy"
+        href="https://tmbgshop.com/"
+        title="Support They Might Be Giants"
+        target="_blank"
+        on:click={() => {
+            ga.addEvent("clickTmbgShop", {
+                name: "clickTmbgShop",
+            });
+        }}
+    >
+        <span class="tmbgshoptext svelte-1d3p4dy"
+            ><img src="/build/tmbglogo.png" alt="TMBG Shop" class="tmbgshopimg mr-2 mb-1 svelte-1d3p4dy" />Support They
+            Might Be Giants </span
+        ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="ml-2"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#FFFFFF"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            ><g fill="none" fill-rule="evenodd"
+                ><path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8" /></g
+            ></svg
+        >
+    </a>
+</div>
