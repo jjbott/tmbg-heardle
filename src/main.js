2700ff80287a8af58b1a108c304b78a026a884bb
Joe Bott <jjbott@gmail.com>
2025-07-10 11:23:57 -0500

    feat: remove migration and any references to old site, since it's gone

--- src/main.js
+++ src/main.js
@@ -1,17 +1,5 @@
-import Game from "./Game.svelte";
 import App from "./App.svelte";
 
-let game = null;
-
-const startGame = (message) => {
-    if (!game) {
-        if (message) {
-            console.error(message);
-        }
-        game = new App({
-            target: document.body
-        });
-    }
-};
-
-startGame();
+new App({
+    target: document.body
+});
