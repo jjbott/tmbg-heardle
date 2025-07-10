71bbc3aee7b7a5ec03b6bceb12903bad0691c3e5
Joe Bott <jjbott@gmail.com>
2025-07-10 11:30:09 -0500

    feat: switch twitter link to bluesky

--- src/InfoModal.svelte
+++ src/InfoModal.svelte
@@ -29,7 +29,7 @@
         <a href="https://tarekraafat.github.io/autoComplete.js/#/">autocomplete.js</a>, and powered by
         <a href="https://pages.cloudflare.com/">Cloudflare</a>. Original Heardle by
         <a href="https://omakase.studio" title="Studio Omakase">Studio omakase / お任せ</a>. TMBG version is currently
-        maintained by <a href="https://twitter.com/ZorMonkey">Joe</a>. Original was created by
+        maintained by <a href="https://bsky.app/profile/joe-bott.bsky.social">Joe</a>. Original was created by
         <a href="https://twitter.com/ckolderup">casey</a>
     </p>
     <p class="text-xs mb-3 text-custom-line">Not affiliated with They Might Be Giants</p>
